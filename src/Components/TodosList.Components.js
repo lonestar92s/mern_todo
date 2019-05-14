dce9c855130eee224475a74e47746cdc4298d914
Andrew Aluko <aluko17@icloud.com>
2019-05-14 10:59:54 -0500

    update

--- src/Components/TodosList.Components.js
+++ src/Components/TodosList.Components.js
@@ -4,9 +4,9 @@ import axios from 'axios';
 
 const Todo = props => (
     <tr>
-        <td>{props.todo.todo_description}</td>
-        <td>{props.todo.todo_responsible}</td>
-        <td>{props.todo.todo_priority}</td>
+        <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_description}</td>
+        <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_responsible}</td>
+        <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_priority}</td>
         <td>
             <Link to={"/edit/"+props.todo._id}>Edit</Link>
         </td>
@@ -19,7 +19,8 @@ export default class TodosList extends Component {
 	state = {
 		todos: []
 	};
-	 componentDidMount() {
+	
+	componentDidMount() {
         axios.get('http://localhost:4000/todos/')
             .then(response => {
                 this.setState({ todos: response.data });
@@ -30,8 +31,8 @@ export default class TodosList extends Component {
     }
 
      todoList() {
-        return this.state.todos.map(function(currentTodo, i){
-            return <Todo todo={currentTodo} key={i} />;
+        return this.state.todos.map(function(currentTodo, index){
+            return <Todo todo={currentTodo} key={index} />;
         })
     }
 
